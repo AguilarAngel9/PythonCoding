3ed0fa4e3d739b796230775313f031c860bc6a52
Angel Aguilar <73602262+AngelAguilar6@users.noreply.github.com>
2022-02-06 12:09:23 -0600

    Add files via upload

---
+++ 201.py
@@ -0,0 +1,13 @@
+def main():
+    print('Do I remember?')
+    return 0
+
+inty = 5
+listy = [6, 7]
+
+striny = 'Hi!'.capitalize()
+
+import folium
+azores = folium.folium.Map(location = (38, -27), zoom_start = 6)
+print(folium.__file__) # Library path.
+# azores.save('output.html')
