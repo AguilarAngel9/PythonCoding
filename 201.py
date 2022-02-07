8928760b169425eb80f67385a1330d4c6487c192
Angel Aguilar <a00827217@itesm.mx>
2022-02-06 18:15:09 -0600

    Update 201.py

--- 201.py
+++ 201.py
@@ -11,3 +11,26 @@ import folium
 azores = folium.folium.Map(location = (38, -27), zoom_start = 6)
 print(folium.__file__) # Library path.
 # azores.save('output.html')
+
+# House Class.
+class House:
+    def __init__(self, wall_area):
+        self.wall_area = wall_area
+
+# Paint Class.
+class Paint:
+    def __init__(self, buckets, color):
+        self.buckets = buckets
+        self.color = color
+    
+    def total_price(self):
+        if self.color == 'white':
+            return self.buckets*1.99
+        else:
+            return self.buckets*2.19
+
+# Person Class.
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
