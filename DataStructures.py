4bcc7f43fedacb5ff660abd8f16d8219f2f60ab8
Angel Aguilar <a00827217@itesm.mx>
2022-03-22 21:54:45 -0600

    Create DataStructures.py

---
+++ DataStructures.py
@@ -0,0 +1,49 @@
+# Bit-wise operators.
+# A brief lecture on the meaning of the bitwise operators.
+
+
+# Complement operator ~
+print(~12) # Used to store negative numbers.
+
+# Bitwise '&'
+print(12 & 13)
+# Is the logical AND comparison of the numbers in binary notation.
+# 12 -> 0 0 0 0 1 1 0 0
+# 13 -> 0 0 0 0 1 1 0 1
+# ----------------------
+#       0 0 0 0 1 1 0 0 -> 12 
+
+# Bitwise '|'
+print(12 | 13)
+# Is the logical OR comparison of the numbers in binary notation.
+# 12 -> 0 0 0 0 1 1 0 0
+# 13 -> 0 0 0 0 1 1 0 1
+# ----------------------
+#       0 0 0 0 1 1 0 1 -> 13
+
+# Bitwise XOR '^'
+print(12 ^ 13)
+# Is the logical XOR comparison of the numbers in binary notation.
+# 12 -> 0 0 0 0 1 1 0 0
+# 13 -> 0 0 0 0 1 1 0 1
+# ----------------------
+#       0 0 0 0 0 0 0 1 -> 1
+
+# Left-shift
+print(10 << 2) # Adding two zeros-bits to the left of the number.
+
+# Right-shift
+print(10 >> 2) # Trimming two zeros-bits to the right of the numb
+
+
+def count_bits(x):
+    num_bits = 0
+    while x:
+        num_bits += x & 1
+        x >>=1
+    return num_bits
+
+
+import sys
+print(sys.maxsize) # Maxsize of an integer.
+print(sys.float_info) # Maxsize of a float.
