604adb44d65705bf2af922375a17a96debd07f9b
Angel Aguilar <anpel.aguiar@gmail.com>
2022-04-10 14:54:03 -0500

    Update 04-10-2022

--- rectangle_game.py
+++ rectangle_game.py
@@ -1,4 +1,6 @@
 from random import randint
+import turtle
+
 
 class Point:
 
@@ -25,21 +27,49 @@ class Rectangle:
                (self.point2.y - self.point1.y)
 
 
-# Create rectangle object
-rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)),
-              Point(randint(10, 19), randint(10, 19)))
+# Inheritance -> class GuiRectangle is a child of Rectangle.
+# GuiRectangle object should have all the attributes if Rectangle.
 
-# Print rectangle coordinates
-print("Rectangle Coordinates: ",
-      rectangle.point1.x, ",",
-      rectangle.point1.y, "and",
-      rectangle.point2.x, ",",
-      rectangle.point2.y)
+class GuiRectangle(Rectangle):
 
-# Get point and area from user
-user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
-user_area = float(input("Guess rectangle area: "))
+    def draw(self, canvas):
+        canvas.penup()
+        canvas.goto(self.point1.x, self.point1.y)
+        canvas.pendown()
 
-# Print out the game result
-print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
-print("Your area was off by: ", rectangle.area() - user_area)
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+        canvas.left(90)
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+        
+        turtle.done() # Keep the Graphics interface
+
+
+gui_rectangle = GuiRectangle(Point(randint(0, 400), randint(0, 400)),
+               Point(randint(10, 400), randint(10, 400)))
+
+myturtle = turtle.Turtle()
+
+gui_rectangle.draw(canvas=myturtle)
+
+# # Create rectangle object
+# rectangle = Rectangle(Point(randint(0, 400), randint(0, 400)),
+#               Point(randint(10, 400), randint(10, 400)))
+
+# # Print rectangle coordinates
+# print("Rectangle Coordinates: ",
+#       rectangle.point1.x, ",",
+#       rectangle.point1.y, "and",
+#       rectangle.point2.x, ",",
+#       rectangle.point2.y)
+
+# # Get point and area from user
+# user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
+# user_area = float(input("Guess rectangle area: "))
+
+# # Print out the game result
+# print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
+# print("Your area was off by: ", rectangle.area() - user_area)
