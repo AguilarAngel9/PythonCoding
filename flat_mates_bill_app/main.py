d214313b611df32e51c3ec2d5feb273a1639bc41
Angel Aguilar <anpel.aguiar@gmail.com>
2022-04-14 21:40:03 -0500

    Updated 04-14-22

---
+++ flat_mates_bill_app/main.py
@@ -0,0 +1,44 @@
+# Flatmates Bill App
+
+class Bill:
+    """ 
+    Object thaat contains data about a bill, such as
+    total amount, and period of the bill.
+    """
+    
+    def __init__(self, amount, period) -> None:
+        self.amount = amount
+        self.period = period
+
+class Flatmate:
+    """
+    Creates a flatname person who live in the flat and pays a share of the bill.
+    """
+
+    def __init__(self, name, days_in_house) -> None:
+        self.name = name
+        self.days_in_house = days_in_house
+    
+    # Methods
+    def pays(self, bill):
+        return bill.amount / 2
+
+class PDFReport:
+    """
+    Creates a PDF file that contain data abour the flatmates such as their names,
+    their due amount and the period of the bill.
+    """
+    
+    def __init__(self, filename) -> None:
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
+        
+
+
+bill = Bill(amount = 120, period = 'March 2020')
+john = Flatmate(name = 'John', days_in_house = 20)
+mary = Flatmate(name = 'Mary', days_in_house = 25)
+
+print(john.pays(bill=bill))
